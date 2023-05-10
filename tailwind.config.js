40430efbf55dc3e2f6b8b27677ca29266aaf6249
DavidDHDH <david.heimfert@gmail.com>
2023-05-10 11:23:03 -0400

    design mobile done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: ['./src/*.{html,js}'],
   theme: {
-    
+
     extend: {
       colors: {
         brand: {
-          light:"#6E9FFB",
-          DEFAULT:"#164398",
-        }
-      }
+          light: '#6E9FFB',
+          DEFAULT: '#164398',
+        },
+      },
     },
   },
   plugins: [],
-}
+};
